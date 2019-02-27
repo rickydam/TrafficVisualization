8f71c1523cddeabe40ca55347e46bdf314de0d3b
Ricky Dam <RickyDamOttawa@gmail.com>
2019-02-27 16:42:30 -0500

    Refactor the visualization JS file

--- visualization.js
+++ visualization.js
@@ -1,42 +1,48 @@
-// To actually be able to display anything with three.js
-// We need three things: scene, camera, and renderer
+function loadVisualization() {
+    // To actually be able to display anything with three.js
+    // We need three things: scene, camera, and renderer
 
-let scene = new THREE.Scene();
-// Params: field of view, aspect ratio, near, far
-let camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+    let scene = new THREE.Scene();
+    // Params: field of view, aspect ratio, near, far
+    let camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 
-let renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+    let renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
 
-let text = 'Hello world!';
-let canvas = createCanvasHD(100, 100);
-let context = canvas.getContext('2d');
-context.font = '20px Arial';
+    let canvas = createCanvas();
+    let texture = new THREE.Texture(canvas);
+    texture.needsUpdate = true;
 
-context.beginPath();
-context.rect(0, 0, 100, 100);
-context.fillStyle = 'gray';
-context.fill();
+    let geometry = new THREE.BoxGeometry(1, 1, 1);
+    let material = new THREE.MeshBasicMaterial({map: texture});
 
-context.fillStyle = 'white';
-context.fillText(text, 0, 20);
-context.strokeStyle = 'white';
-context.strokeText(text, 0, 20);
+    let node = new THREE.Mesh(geometry, material);
+    scene.add(node);
 
-let texture = new THREE.Texture(canvas);
-texture.needsUpdate = true;
+    camera.position.z = 5;
+    renderer.render(scene, camera);
+}
 
-let geometry = new THREE.BoxGeometry(1, 1, 1);
-let material = new THREE.MeshBasicMaterial({map: texture});
+function createCanvas() {
+    let text = 'Hello world!';
+    let canvas = createHDCanvas(100, 100);
+    let context = canvas.getContext('2d');
+    context.font = '20px Arial';
 
-let node = new THREE.Mesh(geometry, material);
-scene.add(node);
+    context.beginPath();
+    context.rect(0, 0, 100, 100);
+    context.fillStyle = 'gray';
+    context.fill();
 
-camera.position.z = 5;
-renderer.render(scene, camera);
+    context.fillStyle = 'white';
+    context.fillText(text, 0, 20);
+    context.strokeStyle = 'white';
+    context.strokeText(text, 0, 20);
+    return canvas;
+}
 
-function createCanvasHD(width, height) {
+function createHDCanvas(width, height) {
     let dpr = window.devicePixelRatio;
     let bspr = 1;
     let ratio = dpr / bspr;
